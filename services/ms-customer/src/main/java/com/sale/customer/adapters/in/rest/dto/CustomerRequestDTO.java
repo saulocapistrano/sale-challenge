23dce256b43745eb2fe22f347a58b04170ab4199
saulocapistrano <saulocapistrano@gmail.com>
2025-04-22 18:33:08 -0300

    feat(rest): create CustomerRequestDTO with validation for name, email and cpf

---
+++ services/ms-customer/src/main/java/com/sale/customer/adapters/in/rest/dto/CustomerRequestDTO.java
@@ -0,0 +1,21 @@
+package com.sale.customer.adapters.in.rest.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Data
+public class CustomerRequestDTO {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @CPF
+    @NotBlank
+    private String cpf;
+}
