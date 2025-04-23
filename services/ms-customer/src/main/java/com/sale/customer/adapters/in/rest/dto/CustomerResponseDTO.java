55a65264ee18ab037b3e5e7c21a540b519676964
saulocapistrano <saulocapistrano@gmail.com>
2025-04-23 15:09:49 -0300

    feat(dto): create response DTO for Customer output mapping

--- services/ms-customer/src/main/java/com/sale/customer/adapters/in/rest/dto/CustomerResponseDTO.java
+++ services/ms-customer/src/main/java/com/sale/customer/adapters/in/rest/dto/CustomerResponseDTO.java
@@ -1,12 +1,21 @@
 package com.sale.customer.adapters.in.rest.dto;
 
 import com.sale.customer.domain.vo.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
-
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerResponseDTO {
     private UUID id;
     private String name;
     private Email email;
     private String cpf;
+    private boolean active;
+
 }
