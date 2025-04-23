e2b7ca2e557c09336c365180aaa99bad07e9026d
saulocapistrano <saulocapistrano@gmail.com>
2025-04-23 14:28:41 -0300

    feat(mapper): add mapper to convert between domain and entity/DTO

---
+++ services/ms-customer/src/main/java/com/sale/customer/adapters/in/rest/mapper/CustomerMapper.java
@@ -0,0 +1,38 @@
+package com.sale.customer.adapters.in.rest.mapper;
+
+import com.sale.customer.adapters.out.persistence.entity.CustomerEntity;
+import com.sale.customer.domain.model.Customer;
+
+
+public class CustomerMapper {
+
+
+    public static Customer toDomain(CustomerEntity entity) {
+        return Customer.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .email(entity.getEmail())
+                .cpf(entity.getCpf())
+                .active(entity.isActive())
+                .build();
+    }
+
+
+    public static CustomerEntity toEntity(Customer customer) {
+        return CustomerEntity.builder()
+                .id(customer.getId())
+                .name(customer.getName())
+                .email(customer.getEmail())
+                .cpf(customer.getCpf())
+                .active(customer.isActive())
+                .build();
+    }
+
+    public static void updateEntity(CustomerEntity entity, Customer customer) {
+        entity.setName(customer.getName());
+        entity.setEmail(customer.getEmail());
+        entity.setCpf(customer.getCpf());
+        entity.setActive(customer.isActive());
+    }
+
+}
