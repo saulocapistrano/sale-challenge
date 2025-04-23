dcff6346d9a095df1bd14434bffa3785c5cbc138
saulocapistrano <saulocapistrano@gmail.com>
2025-04-23 14:29:21 -0300

    feat(usecase): implement delete customer use case

---
+++ services/ms-customer/src/main/java/com/sale/customer/application/ports/usecase/DeleteCustomerUseCaseImpl.java
@@ -0,0 +1,20 @@
+package com.sale.customer.application.ports.usecase;
+
+import com.sale.customer.application.ports.in.DeleteCustomerUseCase;
+import com.sale.customer.application.ports.out.CustomerRepositoryPort;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.util.UUID;
+
+@ApplicationScoped
+public class DeleteCustomerUseCaseImpl implements DeleteCustomerUseCase {
+
+    @Inject
+    CustomerRepositoryPort customerRepositoryPort;
+
+    @Override
+    public void execute(UUID id){
+        customerRepositoryPort.delete(id);
+    }
+}
