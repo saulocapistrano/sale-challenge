3a0b0d43336ce10b1e1ca5f28fec8a018974a24f
saulocapistrano <saulocapistrano@gmail.com>
2025-04-24 17:50:22 -0300

    refactor(dto): adjust CustomerResponseDTO to expose email as string

--- services/ms-customer/src/main/java/com/sale/customer/adapters/in/rest/dto/CustomerResponseDTO.java
+++ services/ms-customer/src/main/java/com/sale/customer/adapters/in/rest/dto/CustomerResponseDTO.java
@@ -1,6 +1,5 @@
 package com.sale.customer.adapters.in.rest.dto;
 
-import com.sale.customer.domain.vo.Email;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,7 +13,7 @@ import java.util.UUID;
 public class CustomerResponseDTO {
     private UUID id;
     private String name;
-    private Email email;
+    private String email;
     private String cpf;
     private boolean active;
 
